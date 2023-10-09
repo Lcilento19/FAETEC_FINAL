822e47018fddf03a986f8821bcc2576bef778f86
Lcilento19 <krony9war@gmail.com>
2023-10-09 20:29:29 -0300

    update choose profile picture

--- projecFinal/src/pages/Home/index.jsx
+++ projecFinal/src/pages/Home/index.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Stopwatch from "../../components/StopWatch";
 import OpenAI from "../../components/OpenAI";
 import Calculator from "../../components/Calculator";
 import "./home.css";
 import { auth, db, storage } from "../../config/firebaseConnection";
 import { signOut } from "firebase/auth";
-import { doc, getDoc, collection } from "firebase/firestore";
+import { doc, getDoc, collection, updateDoc } from "firebase/firestore";
 import "../../components/TemaEscuro/temaEscuro.css";
 import TemaEscuroToggle from "../../components/TemaEscuro/AlternarTema";
 import { useTema } from "../../components/TemaEscuro/TemaContext";
 import { Link } from "react-router-dom";
-import { ref, getDownloadURL } from "firebase/storage";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 
 async function handleLogout() {
   await signOut(auth);
@@ -19,7 +19,8 @@ async function handleLogout() {
 export default function Home() {
   const [userName, setUserName] = useState("");
   const [profilePic, setProfilePic] = useState(null);
-  const [teste, setTeste] = useState("");
+  const [newProfilePic, setNewProfilePic] = useState(null);
+  const fileInputRef = useRef(null);
 
   const user = auth.currentUser;
   const { temaEscuro } = useTema();
@@ -36,11 +37,6 @@ export default function Home() {
           const userData = userDocSnap.data();
           setUserName(userData?.nome || "Usuário");
           setProfilePic(userData?.profilePic);
-          getDownloadURL(ref(storage, "user_notFound.jpeg")).then(
-            (downloadURL) => {
-              setProfilePic(downloadURL);
-            }
-          );
         }
       }
     }
@@ -50,11 +46,53 @@ export default function Home() {
     fetchUserName();
   }, [user]);
 
+  const handleFileChange = (e) => {
+    if (e.target.files[0]) {
+      setNewProfilePic(e.target.files[0]);
+    }
+  };
+
+  const handleUpdateProfilePic = async () => {
+    if (newProfilePic) {
+      const storageRef = ref(storage, `profileImages/${user.uid}`);
+      const uploadTask = uploadBytesResumable(storageRef, newProfilePic);
+
+      try {
+        await uploadTask;
+        const downloadURL = await getDownloadURL(storageRef);
+        setProfilePic(downloadURL);
+        setNewProfilePic(null);
+
+        const userDocRef = doc(collection(db, "contas"), user.uid);
+        await updateDoc(userDocRef, { profilePic: downloadURL });
+      } catch (error) {
+        console.error("Erro ao fazer upload da imagem:", error);
+      }
+    }
+  };
+
   return (
     <div className={`home-container ${temaEscuro ? "dark-theme" : ""}`}>
-      <TemaEscuroToggle temaEscuro={temaEscuro} toggleTema={() => {}} />{" "}
+      <TemaEscuroToggle temaEscuro={temaEscuro} toggleTema={() => {}} />
       <h1 className="title-login">Multi</h1>
       <img src={profilePic} alt="" />
+      {user && user.providerData[0].providerId === "password" && (
+        <>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+            style={{ display: "none" }}
+            ref={fileInputRef}
+          />
+          <button className="button-home" onClick={() => fileInputRef.current.click()}>
+            Escolher Nova Foto
+          </button>
+          {newProfilePic && (
+            <button className="button-home" onClick={handleUpdateProfilePic}>Salvar Nova Foto</button>
+          )}
+        </>
+      )}
       <p className="user">
         Bem-vindo, <span>{userName || "Usuário"}</span>!
       </p>
