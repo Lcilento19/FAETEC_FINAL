dd512a1275a04f634f0908ebf22070e857c1d6df
Lcilento19 <krony9war@gmail.com>
2023-11-14 23:47:59 -0300

    Update index.jsx

--- projecFinal/src/components/OpenAI/index.jsx
+++ projecFinal/src/components/OpenAI/index.jsx
@@ -48,7 +48,7 @@ function OpenAI() {
   const handleGenerateText = async () => {
     try {
       const requestBody = { prompt };
-      const response = await fetch("https://apimultiai.squareweb.app/generate-text", {
+      const response = await fetch("https://chat-api-multi.onrender.com/generate-text", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
