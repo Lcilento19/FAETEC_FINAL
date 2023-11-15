9ae43dca1e29e79ea7f99115d637779c271c141a
Lcilento19 <krony9war@gmail.com>
2023-11-14 23:45:00 -0300

    update api settings

--- Api/index.js
+++ Api/index.js
@@ -4,44 +4,38 @@ import OpenAI from "openai";
 
 import dotenv from "dotenv"; // Importe o dotenv
 
-// Carregue as variáveis de ambiente do arquivo .env
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
-// Configurar a chave da API do OpenAI
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-// Habilitar o middleware CORS
 app.use(cors());
 
 app.use(express.json());
 
-// Rota GET para a raiz do servidor
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the OpenAI API" });
 });
-const conversation = []; // Array para rastrear as mensagens da conversa
+const conversation = [];
 
 app.post("/generate-text", async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    // Adicione a mensagem do usuário à conversa
     conversation.push({ role: "user", content: prompt });
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
         { role: "system", content: "You are a helpful assistant." },
-        ...conversation, // Adicione todas as mensagens da conversa
+        ...conversation,
       ],
     });
 
-    // Adicione a resposta da IA à conversa
     conversation.push({
       role: "assistant",
       content: response.choices[0].message.content,
