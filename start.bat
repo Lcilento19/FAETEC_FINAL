5223aaed00a7b5cdaffb45078682abf41840b83f
Lcilento19 <krony9war@gmail.com>
2023-11-14 23:32:59 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 cd Api
-start cmd   /d "F:\Projetos\Teste_Vite\FAETEC_FINAL\Api" /k node api.js & cd F:\Projetos\Teste_Vite\FAETEC_FINAL\projecFinal & npm run dev
+start cmd   /d "F:\Projetos\Teste_Vite\FAETEC_FINAL\Api" /k node index.js & cd F:\Projetos\Teste_Vite\FAETEC_FINAL\projecFinal & npm run dev
