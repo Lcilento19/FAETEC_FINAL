2280c641ff57e1e7825353199e548185162169f7
Lcilento19 <krony9war@gmail.com>
2023-11-15 11:24:35 -0300

    update folder name projecFianl to client / Api to server

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 cd Api
-start cmd   /d "F:\Projetos\FAETEC_FINAL\Api" /k node index.js & cd F:\Projetos\FAETEC_FINAL\projecFinal & npm run dev
+start cmd   /d "F:\Projetos\FAETEC_FINAL\server" /k node index.js & cd F:\Projetos\FAETEC_FINAL\client & npm run dev
