28fdf301c05bd942574ad891912fdc761ce388ee
Lcilento <107449935+Lcilento19@users.noreply.github.com>
2023-11-30 14:05:43 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({
     message:
-      "Bem vindo aos serviços multi para acessar a função de chat use: https://chat-api-multi.onrender.com//generate-text ",
+      "Welcome to Multi services.\n Use the correct route : https://chat-api-multi.onrender.com//generate-text ",
   });
 });
 const conversation = [];
