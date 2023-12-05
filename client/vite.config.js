8aeddc0018fc810275650d667e92747267ed90f1
Lcilento19 <107449935+Lcilento19@users.noreply.github.com>
2023-12-04 23:26:54 -0300

    update any things

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://192.168.1.66:3001/generate-text",
+        target: "https://chat-api-multi.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
